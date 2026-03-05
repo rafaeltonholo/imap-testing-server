73f2feeec76fba340f75c54a8f775f50dfe6b1ef
Rafael Tonholo <rafael@tonholo.dev>
2026-03-05 15:06:25 -0400

    feat: update entrypoint script to copy DNS configuration for Postfix chroot

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -7,4 +7,10 @@ until nc -z dovecot 24; do
 done
 echo "Dovecot LMTP is up."
 
+# Copy DNS config into the chroot so smtpd can resolve hostnames
+cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+cp /etc/hosts /var/spool/postfix/etc/hosts
+cp /etc/nsswitch.conf /var/spool/postfix/etc/nsswitch.conf
+cp /etc/services /var/spool/postfix/etc/services
+
 exec postfix start-fg
